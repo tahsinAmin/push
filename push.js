b4ff5dbecf67ad30b8a08099ef998a07b6024f3c
Muhammad Tahsin Amin <muhammadtahsinamin@gmail.com>
2022-04-19 09:17:21 +0600

    fix

--- push.js
+++ push.js
@@ -12,6 +12,9 @@ let vapIdKeys = {
 
 push.setVapidDetails('mailto:test@code.co.uk', vapIdKeys.publicKey, vapIdKeys.privateKey)
 
-let sub = {}
+// let sub = {}
+
+// Pretending that it has been pulling it pout of the db
+let sub = { "endpoint": "https://fcm.googleapis.com/fcm/send/cMhv-xZ6SfE:APA91bGkVMHVE0qTBV0Td-IfNMUiU7wYfMpCKQ9Cre8OEaNhFKKATrxl09QFVQpKbjp57BBmLERxSsfWUbFY_1xgdA06YnAIaJn_VpJgp3wJqLDNZvL1jIB4IjvJker7mHER5BitLebP", "expirationTime": null, "keys": { "p256dh": "BE8rGSbiUT0kE03SLemQrXNzU-eA6Gu7G4zvYcaBxqRthuQjsHDUCbV6_dpPB-db0gXGQSyIQXKZmwJ2tUpmsJc", "auth": "HrPjhE1BzXK_ce1F33DOHw" } }
 
 push.sendNotification(sub, 'test-message')
